437d1b02ecb4f97f6bc1b716bf02595e29cd4c52
mohd junaid 55 <129685263+mohdjunaid55@users.noreply.github.com>
2023-04-03 15:02:18 -0700

    Add files via upload

---
+++ ON_OFF.cpp
@@ -0,0 +1,49 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int c;
+	cin>>c;
+	for(int i=0;i<c;i++){
+	    int  n;
+	    cin>>n;
+	    string a;
+	    cin>>a;
+	    string b;
+	    cin>>b;
+	    int p=0;
+	    int q=0;
+	    for(int i=0;i<n;i++){
+	        if(a[i]=='0'){
+	            p++;
+	        }
+	    }
+	    for(int i=0;i<n;i++){
+	        if(b[i]=='0'){
+	            q++;
+	        }
+	    }
+	    
+	    
+	   if(abs(q-p)>=2){
+	    if((abs(q-p))%2==0){
+	        cout<<"1"<<endl;
+	    }
+	    else{
+	        cout<<"0"<<endl;
+	    }
+	    }
+	    else{
+	        if(abs(q-p)==1){
+	            cout<<"0"<<endl;
+	        }
+	        else{
+	            cout<<"1"<<endl;
+	        }
+	    }
+	}
+	return 0;
+}
+
+	
